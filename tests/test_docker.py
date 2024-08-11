b47649414117403f61cedbf457f60f7bb4c0851f
Michał Murawski <mmurawski777@gmail.com>
2024-08-11 13:09:06 +0200

    deug: debugging tests

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -8,6 +8,7 @@ def test_docker():
     client = get_docker_client()
     image = "public.ecr.aws/sam/build-python3.12:latest-x86_64"
     docker_container: Container = get_docker_client().containers.run(
+        "ubuntu", "echo hello world",
         image, tty=True, detach=True
     )
     assert docker_container
